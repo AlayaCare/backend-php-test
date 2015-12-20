f758890b8d7f0a56cc7bb366c132c38b8dec470c
Eric Nogughic <eric_noguchi@hotmail.com>
2015-12-19 20:32:28 -0500

    Task 1 added front end and back end checks for empty todo description

--- src/controllers.php
+++ src/controllers.php
@@ -3,87 +3,98 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-$app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
-    $twig->addGlobal('user', $app['session']->get('user'));
+$app['twig'] = $app->share( $app->extend( 'twig', function($twig, $app) {
+            $twig->addGlobal( 'user', $app['session']->get( 'user' ) );
 
-    return $twig;
-}));
-
-
-$app->get('/', function () use ($app) {
-    return $app['twig']->render('index.html', [
-        'readme' => file_get_contents('README.md'),
-    ]);
-});
+            return $twig;
+        } ) );
 
 
-$app->match('/login', function (Request $request) use ($app) {
-    $username = $request->get('username');
-    $password = $request->get('password');
+$app->get( '/', function () use ($app) {
+    return $app['twig']->render( 'index.html', [
+                'readme' => file_get_contents( 'README.md' ),
+            ] );
+} );
 
-    if ($username) {
+
+$app->match( '/login', function (Request $request) use ($app) {
+    $username = $request->get( 'username' );
+    $password = $request->get( 'password' );
+
+    if ( $username ) {
         $sql = "SELECT * FROM users WHERE username = '$username' and password = '$password'";
-        $user = $app['db']->fetchAssoc($sql);
+        $user = $app['db']->fetchAssoc( $sql );
 
-        if ($user){
-            $app['session']->set('user', $user);
-            return $app->redirect('/todo');
+        if ( $user ) {
+            $app['session']->set( 'user', $user );
+            return $app->redirect( '/todo' );
         }
     }
 
-    return $app['twig']->render('login.html', array());
-});
+    return $app['twig']->render( 'login.html', array() );
+} );
 
 
-$app->get('/logout', function () use ($app) {
-    $app['session']->set('user', null);
-    return $app->redirect('/');
-});
+$app->get( '/logout', function () use ($app) {
+    $app['session']->set( 'user', null );
+    return $app->redirect( '/' );
+} );
 
 
-$app->get('/todo/{id}', function ($id) use ($app) {
-    if (null === $user = $app['session']->get('user')) {
-        return $app->redirect('/login');
-    }
+$app->get( '/todo/{id}', function (Request $request, $id) use ($app) {
+            if ( null === $user = $app['session']->get( 'user' ) ) {
+                return $app->redirect( '/login' );
+            }
 
-    if ($id){
-        $sql = "SELECT * FROM todos WHERE id = '$id'";
-        $todo = $app['db']->fetchAssoc($sql);
+            if ( $id ) {
+                $sql = "SELECT * FROM todos WHERE id = '$id'";
+                $todo = $app['db']->fetchAssoc( $sql );
 
-        return $app['twig']->render('todo.html', [
-            'todo' => $todo,
-        ]);
-    } else {
-        $sql = "SELECT * FROM todos WHERE user_id = '${user['id']}'";
-        $todos = $app['db']->fetchAll($sql);
+                return $app['twig']->render( 'todo.html', [
+                            'todo' => $todo,
+                        ] );
+            } else {
+                $sql = "SELECT * FROM todos WHERE user_id = '${user['id']}'";
+                $todos = $app['db']->fetchAll( $sql );
 
-        return $app['twig']->render('todos.html', [
-            'todos' => $todos,
-        ]);
-    }
-})
-->value('id', null);
+                $template_data = [
+                    'todos' => $todos,
+                ];
+                // passing empty description check to view
+                if ( $request->query->get( 'empty_description' ) ) {
+                    $template_data['empty_description'] = true;
+                }
+
+                return $app['twig']->render( 'todos.html', $template_data );
+            }
+        } )
+        ->value( 'id', null );
 
 
-$app->post('/todo/add', function (Request $request) use ($app) {
-    if (null === $user = $app['session']->get('user')) {
-        return $app->redirect('/login');
+$app->post( '/todo/add', function (Request $request) use ($app) {
+    if ( null === $user = $app['session']->get( 'user' ) ) {
+        return $app->redirect( '/login' );
     }
 
     $user_id = $user['id'];
-    $description = $request->get('description');
+    $description = $request->get( 'description' );
+
+    // if is empty description send back to todo and display error
+    if ( !$description ) {
+        return $app->redirect( '/todo?empty_description=true' );
+    }
 
     $sql = "INSERT INTO todos (user_id, description) VALUES ('$user_id', '$description')";
-    $app['db']->executeUpdate($sql);
+    $app['db']->executeUpdate( $sql );
 
-    return $app->redirect('/todo');
-});
+    return $app->redirect( '/todo' );
+} );
 
 
-$app->match('/todo/delete/{id}', function ($id) use ($app) {
+$app->match( '/todo/delete/{id}', function ($id) use ($app) {
 
     $sql = "DELETE FROM todos WHERE id = '$id'";
-    $app['db']->executeUpdate($sql);
+    $app['db']->executeUpdate( $sql );
 
-    return $app->redirect('/todo');
-});
+    return $app->redirect( '/todo' );
+} );

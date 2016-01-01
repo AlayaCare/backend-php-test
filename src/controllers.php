3dc2aac22feaf50329786bcb9c4d32b06194bd1a
mathieuhentges <mathieuhentges@gmail.com>
2016-01-01 17:30:04 +0100

    Task-2 User can mark a Todo as done (but didn t add a column yet to show that the task has been done

--- src/controllers.php
+++ src/controllers.php
@@ -84,6 +84,14 @@ $app->post('/todo/add', function (Request $request) use ($app) {
 });
 
 
+$app->match('/todo/completed/{id}', function ($id) use ($app) {
+
+    $sql = "UPDATE todos SET completed = 1 WHERE id ='$id'";
+    $app['db']->executeUpdate($sql);
+
+    return $app->redirect('/todo');
+});
+
 $app->match('/todo/delete/{id}', function ($id) use ($app) {
 
     $sql = "DELETE FROM todos WHERE id = '$id'";

f80a27c1bb954d4b6ba19b350c36a1b638ecb22f
Guillaume Boivin <guil.boivin@gmail.com>
2016-08-09 23:00:02 -0400

    TASK-4 : Show a confirmation message on todo add/delete.

--- src/controllers.php
+++ src/controllers.php
@@ -91,6 +91,7 @@ $app->post('/todo/add', function (Request $request) use ($app) {
     else{
     	$sql = "INSERT INTO todos (user_id, description) VALUES ('$user_id', '$description')";
     	$app['db']->executeUpdate($sql);
+    	$app['session']->getFlashBag()->add('todo_confirmation', "Todo successfully added.");
     }
 
     return $app->redirect('/todo');
@@ -125,6 +126,8 @@ $app->match('/todo/delete/{id}', function ($id) use ($app) {
 
     $sql = "DELETE FROM todos WHERE id = '$id'";
     $app['db']->executeUpdate($sql);
+    
+    $app['session']->getFlashBag()->add('todo_confirmation', "Todo successfully deleted.");
 
     return $app->redirect('/todo');
 });

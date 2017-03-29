51f4c60bf4efd8c880b1c0eb8b8072067d1cfd7e
redunderthebed <redunderthebed@gmail.com>
2017-03-28 22:20:42 -0400

    Task 5: Pagination implemented on todos index page, also using ORM

--- src/controllers.php
+++ src/controllers.php
@@ -2,6 +2,7 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Entity\Todo;
 use Entity\User;
 
@@ -47,11 +48,11 @@ $app->get('/logout', function () use ($app) {
 });
 
 
-$app->get('/todo/{id}', function ($id) use ($app) {
+$app->get('/todo/{id}', function (Request $request, $id) use ($app) {
     if (null === $user = $app['session']->get('user')) {
         return $app->redirect('/login');
     }
-
+    $user_id = $app['session']->get('user')->getId();
     if ($id){
         $em = $app['db.orm.em'];
         $todo = $em->getRepository('Entity\Todo')->findBy(
@@ -66,13 +67,20 @@ $app->get('/todo/{id}', function ($id) use ($app) {
     } else {
         
         $em = $app['db.orm.em'];
+        $current_page = $request->get('current_page');
+        if(!$current_page){
+            $current_page = 1;
+        }
+        $query = $em->createQuery("select t from Entity\Todo t where t.user_id = " . $user_id);
         $todos = $em->getRepository('Entity\Todo')->findBy(
             array( 
                 "user_id" => $app['session']->get('user')->getId()
             ));
-
+        $pageSize = 10;
+        $paginator = new Paginator($query);
+        $paginator->getQuery()->setMaxResults($pageSize)->setFirstResult($pageSize * ($current_page - 1));
         return $app['twig']->render('todos.html', [
-            'todos' => $todos,
+            'todos' => $paginator,
         ]);
     }
 })

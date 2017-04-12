30817ba3e27843843d0fbcee579603cb01e7bb98
Ara <gohabs4ever@gmail.com>
2017-04-12 16:35:16 -0400

    TASK-4: Used flashback to notify user when a task was created, completed and deleted

--- src/controllers.php
+++ src/controllers.php
@@ -75,6 +75,8 @@ $app->post('/todo/add', function (Request $request) use ($app) {
 
     $sql = "INSERT INTO todos (user_id, description) VALUES ('$user_id', '$description')";
     $app['db']->executeUpdate($sql);
+	
+	$app['session']->getFlashBag()->add('messages', ['type' => 'success', 'message' => 'Successfully added a new task!']);
 
     return $app->redirect('/todo');
 });
@@ -83,6 +85,10 @@ $app->match('/todo/completed/{id}', function ($id) use ($app) {
 	
 	$sql = "UPDATE todos SET completed = 1 WHERE id = '$id'";
 	$app['db']->executeUpdate($sql);
+	
+	$app['session']->getFlashBag()->add('messages', ['type'    => 'success',
+	                                                 'message' => 'Successfully completed the task!'
+	]);
 	
 	return $app->redirect('/todo');
 });
@@ -100,6 +106,10 @@ $app->match('/todo/delete/{id}', function ($id) use ($app) {
 
     $sql = "DELETE FROM todos WHERE id = '$id'";
     $app['db']->executeUpdate($sql);
+	
+	$app['session']->getFlashBag()->add('messages', ['type'    => 'success',
+	                                                 'message' => 'Successfully deleted the task!'
+	]);
 
     return $app->redirect('/todo');
 });

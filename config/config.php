449042b1091692cbc22c4978e3a299c3357aed72
Jeevitha1234 <jeevitham001@gmail.com>
2017-08-10 12:04:39 -0400

    test
    tes

--- config/config.php
+++
@@ -1,9 +0,0 @@
-$container->loadFromExtension('doctrine', array(
-    'dbal' => array(
-        'driver'   => 'pdo_mysql',
-        'host'     => '%localhost%',
-        'dbname'   => '%ac_todos%',
-        'user'     => '%root%',
-        'password' => '%%',
-    ),
-));

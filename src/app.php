dc7bc16349af33ea2fddfb90d68372d73ea07869
djleven <info@e-leven.net>
2018-01-06 05:59:20 +0000

    Task-0.4: Refactor app.php into a class

--- src/app.php
+++ src/app.php
@@ -10,20 +10,85 @@ use Silex\Provider\DoctrineServiceProvider;
 use Silex\Provider\AssetServiceProvider;
 use Lokhman\Silex\Provider\ConfigServiceProvider;
 
-$app = new Application();
-$app->register(new SessionServiceProvider());
-$app->register(new ValidatorServiceProvider());
-$app->register(new ServiceControllerServiceProvider());
-$app->register(new TwigServiceProvider());
-$app->register(new HttpFragmentServiceProvider());
 
-$app->register(new ConfigServiceProvider(),array('config.dir' => __DIR__ . '/../config/'));
-$app->register(new DoctrineServiceProvider, array(
-    'db.options' => $app['config']['database'],
-));
-$app->register(new AssetServiceProvider(), array(
-    'assets.version' => $app['config']['assets']['version'],
-    'assets.version_format' => $app['config']['assets']['version_format'],
-    'assets.named_packages' => $app['config']['assets']['named_packages'],
-));
+class App extends Silex\Application
+
+{
+    /**
+     * Class constructor
+     *
+     * Registering the App Service Providers
+     */
+    public function __construct()
+
+    {
+        parent::__construct();
+
+        $this->registerConfigServiceProvider();
+        $this->registerDoctrineServiceProvider();
+        $this->registerAssetServiceProvider();
+        $this->registerOtherServiceProviders();
+    }
+
+    /**
+     * Register Config service provider
+     *
+     *  If a web server deployment environment 'SILEX_ENV' is not setup, it will default to local:
+     *  $app['config.env.default'] = 'local';
+     *  $app['config.varname.default'] = 'SILEX_ENV';
+     *  You can override both server and default values by uncommenting/setting the 'config.env' below
+     *  See https://github.com/lokhman/silex-config under Global environment variable and public function register(Container $app))
+     */
+    private function registerConfigServiceProvider()
+    {
+        $this->register(new ConfigServiceProvider() , ['config.dir' => __DIR__ . '/../config/',
+        // 'config.env' => 'prod',
+        ]);
+    }
+
+    /**
+     * Register Doctrine service provider
+     *
+     */
+    private function registerDoctrineServiceProvider()
+    {
+        $this->register(new DoctrineServiceProvider, array(
+            'db.options' => $this['config']['database'],
+        ));
+    }
+
+    /**
+     * Register asset service provider
+     *
+     */
+    private function registerAssetServiceProvider()
+    {
+        $app = $this;
+        $app->register(new AssetServiceProvider() , array(
+            'assets.version' => $app['config']['assets']['version'],
+            'assets.version_format' => $app['config']['assets']['version_format'],
+            'assets.named_packages' => $app['config']['assets']['named_packages'],
+        ));
+    }
+
+    /**
+     * Register other service providers
+     *
+     * Well since we're not passing any parameters with these, let's just register them in one go for now
+     */
+    private function registerOtherServiceProviders()
+    {
+        $app = $this;
+        $app->register(new ServiceControllerServiceProvider());
+        $app->register(new SessionServiceProvider());
+        $app->register(new ValidatorServiceProvider());
+        $app->register(new TwigServiceProvider());
+        $app->register(new HttpFragmentServiceProvider());
+
+    }
+}
+
+
+$app = new App();
+
 return $app;

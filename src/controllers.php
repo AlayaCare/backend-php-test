edb4d6d4c9ae818cf9bee3761a00413e684e82fe
Karim Wallani <karim.wallani@gmail.com>
2018-01-11 01:10:19 -0500

    TASK-1: Rejects tasks without descriptions. Tasks with descriptions are inserted securely.

--- src/controllers.php
+++ src/controllers.php
@@ -73,8 +73,24 @@ $app->post('/todo/add', function (Request $request) use ($app) {
     $user_id = $user['id'];
     $description = $request->get('description');
 
-    $sql = "INSERT INTO todos (user_id, description) VALUES ('$user_id', '$description')";
-    $app['db']->executeUpdate($sql);
+    // Task 1
+    if (trim($description) == "") {
+        $sql = "SELECT * FROM todos WHERE user_id = '${user['id']}'";
+        $todos = $app['db']->fetchAll($sql);
+
+        return $app['twig']->render('todos.html', [
+            'error' => 'Please provide a task description!',
+            'todos' => $todos,
+        ]);
+    }
+
+    // Extra task
+    //$sql = "INSERT INTO todos (user_id, description) VALUES ('$user_id', '$description')";
+    //$app['db']->executeUpdate($sql);
+    $app['db']->insert('todos', array(
+        'user_id' => $user_id,
+        'description' => $description
+    ));
 
     return $app->redirect('/todo');
 });

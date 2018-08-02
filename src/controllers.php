1f83b6b899651e81c2c69ea0cbc6dc25142d7c39
shashank <shashank.singh>
2018-08-02 19:05:09 +0530

    user can see a confirmation message when adding/deleteting a todo

--- src/controllers.php
+++ src/controllers.php
@@ -54,7 +54,7 @@ $app->get('/todo/{id}', function ($id) use ($app) {
             'todo' => $todo,
         ]);
     } else {
-        $sql = "SELECT * FROM todos WHERE user_id = '${user['id']}' ";
+        $sql = "SELECT * FROM todos WHERE user_id = '${user['id']}' AND completed!=1";
         $todos = $app['db']->fetchAll($sql);
 
         return $app['twig']->render('todos.html', [
@@ -72,34 +72,52 @@ $app->post('/todo/add', function (Request $request) use ($app) {
 
     $user_id = $user['id'];
     $description = $request->get('description');
-    if (!empty($description) && !empty($user_id)) {
-        $sql = "INSERT INTO todos (user_id, description) VALUES ('$user_id', '$description')";
-        $app['db']->executeUpdate($sql);
-        $app['session']->getFlashBag()->add('message', 'Todo successfully added.');
-    } else {
-        $app['session']->getFlashBag()->add('message', 'Either description or user is emppty.');
+
+    if(($user_id !=null) and ($description !=null)){
+        try{
+            $sql = "INSERT INTO todos (user_id, description) VALUES ('$user_id', '$description')";
+            $app['db']->executeUpdate($sql);
+            $app['session']->getFlashBag()->add('success', 'Todo added successfully');
+        }
+        catch(Exception $e){
+            $app['session']->getFlashBag()->add('error', 'There has been an error. Try again.');
+        }
     }
+
     return $app->redirect('/todo');
 });
 
 
 $app->match('/todo/delete/{id}', function ($id) use ($app) {
 
-    $sql = "DELETE FROM todos WHERE id = '$id'";
-    $app['db']->executeUpdate($sql);
+    try{
+        $sql = "DELETE FROM todos WHERE id = '$id'";
+        $app['db']->executeUpdate($sql);
+        $app['session']->getFlashBag()->add('success', 'Todo deleted');
+    }
+    catch(Exception $e){
+        $app['session']->getFlashBag()->add('error', 'There has been an error. Try again.');
+    }
 
     return $app->redirect('/todo');
 });
 
 $app->match('/todo/mark/{id}', function ($id) use ($app) {
-    $now = date_create('now')->format('Y-m-d H:i:s');
-    $sql = "UPDATE todos SET completed=1, date_completed='" . $now . "' WHERE id = '$id'";
-    $app['db']->executeUpdate($sql);
+    try{
+        $now = date_create('now')->format('Y-m-d H:i:s');
+        $sql = "UPDATE todos SET completed=1, date_completed='" . $now . "' WHERE id = '$id'";
+        $app['db']->executeUpdate($sql);
+        $app['session']->getFlashBag()->add('success', 'Todo completed');
+     }
+    catch(Exception $e){
+        $app['session']->getFlashBag()->add('error', 'There has been an error. Try again.');
+    }
 
     return $app->redirect('/todo');
 });
 
 $app->match('/todo/{id}/json', function ($id) use ($app) {
+
     $sql = "SELECT id, user_id, description FROM todos WHERE id = '$id'";
     $todo = $app['db']->fetchAssoc($sql);
     return json_encode($todo);

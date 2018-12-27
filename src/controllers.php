e8f7b15c0770b3fd295f58117ef2faa8009b0f78
Andrey Nagy <andreynagy@gmail.com>
2018-12-27 00:55:22 -0500

    TASK 3: AS a user I can view a todo in a JSON format (SOLVED). now you can a todo in a json format in todo.html or navigating on the todoList in todoJSON.html

--- src/controllers.php
+++ src/controllers.php
@@ -6,10 +6,8 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 $app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
     $twig->addGlobal('user', $app['session']->get('user'));
-
     return $twig;
 }));
-
 
 $app->get('/', function () use ($app) {
       return $app['twig']->render('index.html', [
@@ -17,42 +15,35 @@ $app->get('/', function () use ($app) {
     ]);
 });
 
-
 $app->match('/login', function (Request $request) use ($app) {
     $username = $request->get('username');
-    $password = $request->get('password');
-    
+    $password = $request->get('password');    
     if ($username) {
         $sql = "SELECT * FROM users WHERE username = '$username' and password = '$password'";
         $user = $app['db']->fetchAssoc($sql);
-
         if ($user){
             $app['session']->set('user', $user);
             return $app->redirect('/todo');
         }
     }
-
     return $app['twig']->render('login.html', array());
 });
-
 
 $app->get('/logout', function () use ($app) {
     $app['session']->set('user', null);
     return $app->redirect('/');
 });
 
-
 $app->get('/todo/{id}', function ($id) use ($app) {
     if (null === $user = $app['session']->get('user')) {
         return $app->redirect('/login');
     }
-
     if ($id){
         $sql = "SELECT * FROM todos WHERE id = '$id'";
         $todo = $app['db']->fetchAssoc($sql);
-
         return $app['twig']->render('todo.html', [
             'todo' => $todo,
+            'todoJson' => json_encode($todo, JSON_PRETTY_PRINT),
         ]);
     } else {
         $sql = "SELECT * FROM todos WHERE user_id = '${user['id']}'";
@@ -65,20 +56,15 @@ $app->get('/todo/{id}', function ($id) use ($app) {
 })
 ->value('id', null);
 
-
 $app->post('/todo/add', function (Request $request) use ($app) {
     if (null === $user = $app['session']->get('user')) {
         return $app->redirect('/login');
     }
-
     $user_id = $user['id'];
-    $description = $request->get('description');
-    
-    $errors = $app['validator']->validate($description, new Assert\NotBlank());         
-     
+    $description = $request->get('description');    
+    $errors = $app['validator']->validate($description, new Assert\NotBlank());     
     if (count($errors) > 0) {
-        $app['session']->getFlashBag()->add("INFO", "Description is required"); 
-          
+        $app['session']->getFlashBag()->add("INFO", "Description is required");           
     } else {
         $sql = "INSERT INTO todos (user_id, description) VALUES ('$user_id', '$description')";
         $app['db']->executeUpdate($sql);
@@ -89,24 +75,26 @@ $app->post('/todo/add', function (Request $request) use ($app) {
 
 
 $app->match('/todo/delete/{id}', function ($id) use ($app) {
-
     $sql = "DELETE FROM todos WHERE id = '$id'";
     $app['db']->executeUpdate($sql);
-
     return $app->redirect('/todo');
 });
 
-$app->post('/todo/completed/{id}/{completed}', function ($id,$completed) use ($app) {
-    
+$app->post('/todo/completed/{id}/{completed}', function ($id,$completed) use ($app) {    
     if (null === $user = $app['session']->get('user')) {
        return $app->redirect('/login');
-    }      
-    
-    ($completed==0) ? $completed = 1: $completed = 0;   
-    
+    }        
+    ($completed==0) ? $completed = 1: $completed = 0;       
     $sql = "UPDATE todos SET completed = '$completed' WHERE id = '$id'";
     $app['db']->executeUpdate($sql);
     $app['session']->getFlashBag()->add("SUCCESS", "Todo was updated!"); 
-
     return $app->redirect('/todo');
+});
+
+$app->get('/todo/{id}/json', function ($id) use ($app) {
+    $sql = "SELECT * FROM todos WHERE id = '$id'";
+    $todo = $app['db']->fetchAssoc($sql);
+    return $app['twig']->render('todoJSON.html', [
+       'todoJson' => json_encode($todo, JSON_PRETTY_PRINT),
+    ]);
 });

b223e31364fe1825e8b00d06382e7976d05c0981
Andre Nunes <andrenba@gmail.com>
2019-08-29 01:55:21 -0300

    TASK 3: As a user I can view a todo in a JSON format.

--- src/controllers.php
+++ src/controllers.php
@@ -137,3 +137,18 @@ $app->match('/todo/done/{id}', function ($id) use ($app) {
 
     return $app->redirect('/todo');
 });
+
+
+$app->get('/todo/{id}/json', function ($id) use ($app) {
+
+    if (null === $user = $app['session']->get('user')) {
+        return $app->redirect('/login');
+    }
+    
+    if ($id){
+
+        $sql = "SELECT * FROM todos WHERE id = '$id'";
+        $todo = $app['db']->fetchAssoc($sql);
+        return json_encode($todo);
+    }
+});

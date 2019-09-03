0ced708a20c057e7a95ee935e3688fb5cca676f9
Paulo Oliveira <paulinho@Paulos-iMac.local>
2019-09-03 14:08:10 -0400

    TASK-5: Including pagination on Todo List when needed

--- src/controllers.php
+++ src/controllers.php
@@ -3,6 +3,8 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Kosinix\Paginator;
+use Kosinix\Pagination;
 
 $app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
     $twig->addGlobal('user', $app['session']->get('user'));
@@ -28,7 +30,7 @@ $app->match('/login', function (Request $request) use ($app) {
 
         if ($user){
             $app['session']->set('user', $user);
-            return $app->redirect('/todo');
+            return $app->redirect('/todos');
         }
     }
 
@@ -47,23 +49,53 @@ $app->get('/todo/{id}', function ($id) use ($app) {
         return $app->redirect('/login');
     }
 
-    if ($id){
-        $sql = "SELECT * FROM todos WHERE id = '$id'";
-        $todo = $app['db']->fetchAssoc($sql);
+    $sql = "SELECT * FROM todos WHERE id = '$id'";
+    $todo = $app['db']->fetchAssoc($sql);
 
-        return $app['twig']->render('todo.html', [
-            'todo' => $todo,
-        ]);
-    } else {
-        $sql = "SELECT * FROM todos WHERE user_id = '${user['id']}'";
-        $todos = $app['db']->fetchAll($sql);
-
-        return $app['twig']->render('todos.html', [
-            'todos' => $todos,
-        ]);
-    }
+    return $app['twig']->render('todo.html', [
+        'todo' => $todo,
+    ]);
 })
 ->value('id', null);
+
+
+$app->get('/todos/{page}/{sort_by}/{sorting}', function (Request $request, $page, $sort_by, $sorting) use ($app) {
+    if (null === $user = $app['session']->get('user')) {
+        return $app->redirect('/login');
+    }
+
+    $sql = 'SELECT COUNT(*) AS `total` FROM todos WHERE user_id='.$user['id'] ;
+    $count = $app['db']->fetchAssoc($sql);
+    $count = (int) $count['total'];
+
+    /** @var \Kosinix\Paginator $paginator */
+    $paginator =  $app['paginator']($count, $page);
+
+    $sql = sprintf('SELECT * FROM todos WHERE user_id='. $user['id']. '
+                    ORDER BY %s %s
+                    LIMIT %d,%d',
+            $sort_by, 
+            strtoupper($sorting), 
+            $paginator->getStartIndex(), 
+            $paginator->getPerPage());
+
+    $todos = $app['db']->fetchAll($sql);
+
+    $pagination = new Pagination($paginator, $app['url_generator'], 'templates', $sort_by, $sorting);
+
+    return $app['twig']->render('todos.html', array(
+        'todos' => $todos,
+        'pagination' => $pagination
+    ));
+})
+->value('page', 1)
+->value('sort_by', 'id')
+->value('sorting', 'asc')
+->assert('page', '\d+') // Numbers only
+->assert('sort_by','[a-zA-Z_]+') // Match a-z, A-Z, and "_"
+->assert('sorting','(\basc\b)|(\bdesc\b)') // Match "asc" or "desc"
+->bind('templates');
+
 
 $app->get('/todo/{id}/json', function ($id) use ($app) {
     if (null === $user = $app['session']->get('user')) {
@@ -110,7 +142,7 @@ $app->post('/todo/add', function (Request $request) use ($app) {
     } else {
         $app['session']->getFlashBag()->add('error_messages', 'Error! A ToDo can\'t be created without a description.');
     }
-    return $app->redirect('/todo');
+    return $app->redirect('/todos');
 });
 
 $app->post('/todo/done/{id}', function ($id) use ($app) {
@@ -121,7 +153,7 @@ $app->post('/todo/done/{id}', function ($id) use ($app) {
     $sql = "UPDATE todos SET status = 1 WHERE id = '$id'";
     $app['db']->executeUpdate($sql);
 
-    return $app->redirect('/todo');
+    return $app->redirect('/todos');
 });
 
 
@@ -133,7 +165,7 @@ $app->post('/todo/undone/{id}', function ($id) use ($app) {
     $sql = "UPDATE todos SET status = 0 WHERE id = '$id'";
     $app['db']->executeUpdate($sql);
 
-    return $app->redirect('/todo');
+    return $app->redirect('/todos');
 }); 
 
 
@@ -166,5 +198,5 @@ $app->match('/todo/delete/{id}', function ($id) use ($app) {
         $app['session']->getFlashBag()->add('error' , 'You need to provide an id.');
     }
 
-    return $app->redirect('/todo');
+    return $app->redirect('/todos');
 });
